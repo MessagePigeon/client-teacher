585ab4ed39e31072c8af4f3cdca6de2e8a69291c
Justin Sun <i@jsun969.cn>
2022-02-08 15:16:42 +0800

    feat(auth): add auth pages basic layout

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,95 @@
+import { LockOutlined, Visibility, VisibilityOff } from '@mui/icons-material';
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  IconButton,
+  InputAdornment,
+  Link,
+  TextField,
+  Typography,
+} from '@mui/material';
 import React from 'react';
+import { useBoolean } from 'ahooks';
 
 const Login: React.FC = () => {
-  return <div>登录</div>;
+  const [showPassword, { toggle }] = useBoolean();
+
+  return (
+    <Box
+      sx={{
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <LockOutlined />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        登录
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={() => console.log('submit')}
+        noValidate
+        sx={{ mt: 1 }}
+      >
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="用户名"
+          name="username"
+          autoComplete="username"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="密码"
+          type={showPassword ? 'text' : 'password'}
+          id="password"
+          autoComplete="current-password"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={toggle}>
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="记住我"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          登录
+        </Button>
+        <Grid container>
+          <Grid item xs>
+            <Link variant="body2" component="button">
+              忘记密码
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
 };
 
 export default Login;
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,7 +1,116 @@
+import { LockOutlined, Visibility, VisibilityOff } from '@mui/icons-material';
+import {
+  Avatar,
+  Box,
+  Button,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { useBoolean } from 'ahooks';
 import React from 'react';
 
 const Register: React.FC = () => {
-  return <div>注册</div>;
+  const [showPassword, { toggle }] = useBoolean();
+
+  return (
+    <Box
+      sx={{
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <LockOutlined />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        注册
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={() => console.log('submit')}
+        noValidate
+        sx={{ mt: 1 }}
+      >
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="用户名"
+          name="username"
+          autoComplete="username"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="name"
+          label="真实姓名"
+          name="name"
+          autoComplete="name"
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="密码"
+          type={showPassword ? 'text' : 'password'}
+          id="password"
+          autoComplete="new-password"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={toggle}>
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="re-password"
+          label="重复密码"
+          type={showPassword ? 'text' : 'password'}
+          id="re-password"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={toggle}>
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="register-code"
+          label="注册码"
+          type="password"
+          id="register-code"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          注册
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default Register;
