80f566957a5b2b1b48d8f563160f8533d614b47f
Justin Sun <i@jsun969.cn>
2022-02-11 19:50:08 +0800

    feat(vite): add backend api proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: { '@material-ui/icons': '@material-ui/icons/esm' },
-  },
-});
+export default ({ mode }) => {
+  const { VITE_API_URL } = loadEnv(mode, './');
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: { '@material-ui/icons': '@material-ui/icons/esm' },
+    },
+    server: {
+      proxy: {
+        '/teacher': VITE_API_URL,
+      },
+    },
+  });
+};
