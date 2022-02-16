e079a83de6d0fda206a6f9480cb635e0907581d1
Justin Sun <i@jsun969.cn>
2022-02-16 14:01:32 +0800

    feat(layout/user): add network error modal instead of toast hook

--- src/layout/UserLayout.tsx
+++ src/layout/UserLayout.tsx
@@ -7,40 +7,44 @@ import {
 import { Box } from '@mui/material';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import NetworkErrorModal from './components/NetworkErrorModal';
 import LayoutBase from './LayoutBase';
 
 const UserLayout: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <LayoutBase
-      navigation={[
-        { title: '发送消息', icon: <ForwardToInbox />, path: 'send-message' },
-        { title: '历史记录', icon: <History />, path: 'history' },
-        { title: '添加设备', icon: <Devices />, path: 'add-device' },
-        {
-          title: '个人设置',
-          icon: <ManageAccountsOutlined />,
-          path: 'personal-settings',
-        },
-      ]}
-      isLogin
-      logout={() => navigate('login')}
-      teacherName="XXX"
-      MainContainer={({ children }) => (
-        <Box
-          component="main"
-          sx={{
-            mt: 10,
-            mb: { xs: 10, md: 2 },
-            ml: { xs: 2, md: '216px' },
-            mr: 2,
-          }}
-        >
-          {children}
-        </Box>
-      )}
-    />
+    <>
+      <LayoutBase
+        navigation={[
+          { title: '发送消息', icon: <ForwardToInbox />, path: 'send-message' },
+          { title: '历史记录', icon: <History />, path: 'history' },
+          { title: '添加设备', icon: <Devices />, path: 'add-device' },
+          {
+            title: '个人设置',
+            icon: <ManageAccountsOutlined />,
+            path: 'personal-settings',
+          },
+        ]}
+        isLogin
+        logout={() => navigate('login')}
+        teacherName="XXX"
+        MainContainer={({ children }) => (
+          <Box
+            component="main"
+            sx={{
+              mt: 10,
+              mb: { xs: 10, md: 2 },
+              ml: { xs: 2, md: '216px' },
+              mr: 2,
+            }}
+          >
+            {children}
+          </Box>
+        )}
+      />
+      <NetworkErrorModal />
+    </>
   );
 };
 
---
+++ src/layout/components/NetworkErrorModal.tsx
@@ -0,0 +1,31 @@
+import { Alert, Backdrop } from '@mui/material';
+import { useBoolean, useNetwork, useUpdateEffect } from 'ahooks';
+import React from 'react';
+
+const NetworkErrorModal: React.FC = () => {
+  const { online } = useNetwork();
+  const [shouldRefresh, { set: setShouldRefresh }] = useBoolean();
+
+  useUpdateEffect(() => {
+    if (online && shouldRefresh) {
+      // refresh page to avoid losing websocket message
+      location.reload();
+    }
+    if (!online) {
+      setShouldRefresh(true);
+    }
+  }, [online, shouldRefresh]);
+
+  return (
+    <Backdrop
+      open={!online}
+      sx={{ zIndex: (theme) => theme.zIndex.drawer + 999, color: '#fff' }}
+    >
+      <Alert severity="warning" variant="filled">
+        Network Error
+      </Alert>
+    </Backdrop>
+  );
+};
+
+export default NetworkErrorModal;
