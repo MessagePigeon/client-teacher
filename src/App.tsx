0c5cbcce431e0b1c91feea3e95dff7b65bab608a
Justin Sun <i@jsun969.cn>
2022-02-19 23:14:07 +0800

    feat(state): init `recoil`

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Routers from './pages/Routers';
+import { RecoilRoot } from 'recoil';
 
 const theme = createTheme({ palette: { primary: indigo, secondary: pink } });
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Routers />
-      <ToastContainer position="top-center" theme="colored" newestOnTop />
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routers />
+        <ToastContainer position="top-center" theme="colored" newestOnTop />
+      </ThemeProvider>
+    </RecoilRoot>
   );
 };
 
