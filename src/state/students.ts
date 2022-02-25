edbeff3c1bd526211fffe429288e962a445c272e
Justin Sun <i@jsun969.cn>
2022-02-26 00:57:13 +0800

    feat(state/students): export student type

--- src/state/students.ts
+++ src/state/students.ts
@@ -1,6 +1,6 @@
 import { atom } from 'recoil';
 
-type Student = { id: string; online: boolean; remark: string };
+export type Student = { id: string; online: boolean; remark: string };
 
 export const connectedStudentsState = atom<Student[]>({
   key: 'connectedStudents',
