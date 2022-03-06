ee44ee101025957da7499136281743cb04f21796
Justin Sun <i@jsun969.cn>
2022-03-06 20:17:54 +0800

    fix(ws): remove error websocket with recoil

--- src/hooks/use-recoil-value-in-custom-hook.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect, useRef } from 'react';
-
-export default function useRecoilValueInCustomHook<T>(stateValue: T) {
-  const latestValue = useRef(stateValue);
-  useEffect(() => {
-    latestValue.current = stateValue;
-  }, [stateValue]);
-  return latestValue.current;
-}
