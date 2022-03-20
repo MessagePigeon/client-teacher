99053d41f3e725884168b8e1cba58ac092a09312
Justin Sun <i@jsun969.cn>
2022-03-20 16:34:31 +0800

    chore(eslint): include tsconfig file of `/apps`

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './packages/*/tsconfig.json',
+    project: ['./apps/*/tsconfig.json', './packages/*/tsconfig.json'],
   },
   plugins: ['@typescript-eslint'],
   extends: [
