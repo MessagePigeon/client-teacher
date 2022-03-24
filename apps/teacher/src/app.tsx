9d70af52d03327672be56b944317c0a636eb99e2
Justin Sun <i@jsun969.cn>
2022-03-24 08:46:26 +0800

    fix: remove `RecoilRoot`

--- apps/teacher/src/app.tsx
+++ apps/teacher/src/app.tsx
@@ -3,20 +3,17 @@ import { indigo, pink } from '@mui/material/colors';
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { RecoilRoot } from 'recoil';
 import Routers from './routers';
 
 const theme = createTheme({ palette: { primary: indigo, secondary: pink } });
 
 const App: React.FC = () => {
   return (
-    <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Routers />
-        <ToastContainer position="top-center" theme="colored" newestOnTop />
-      </ThemeProvider>
-    </RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routers />
+      <ToastContainer position="top-center" theme="colored" newestOnTop />
+    </ThemeProvider>
   );
 };
 
