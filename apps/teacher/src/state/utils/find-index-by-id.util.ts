ff831d9e2db119040d3f96911c3757ee57d6e047
Char2s <Char2s@outlook.com>
2022-03-24 12:12:11 +0800

    refactor: enhance type of `findIndexById`

--- apps/teacher/src/state/utils/find-index-by-id.util.ts
+++ apps/teacher/src/state/utils/find-index-by-id.util.ts
@@ -1,7 +1,14 @@
-export function findIndexById<TElement extends Record<string, any>, TIdValue>(
+export function findIndexById<
+  TElement extends Record<TIdKey, any>,
+  TIdKey extends string
+>(array: TElement[], id: TElement[TIdKey], idKey: TIdKey);
+export function findIndexById<TElement extends { id: unknown }>(
   array: TElement[],
-  id: TIdValue,
-  idKey: TElement extends { id: TIdValue } ? 'id' : keyof TElement,
-) {
+  id: TElement["id"]
+);
+export function findIndexById<
+  TElement extends Record<TIdKey, any>,
+  TIdKey extends string
+>(array: TElement[], id: TElement[TIdKey], idKey: TIdKey = "id" as TIdKey) {
   return array.findIndex((element) => element[idKey] === id);
 }
