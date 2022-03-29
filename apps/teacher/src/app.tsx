da3ea4732f7c3282a9df6a6396dcf8f9f98410a9
Justin Sun <i@jsun969.cn>
2022-03-29 21:53:10 +0800

    feat: add title i18n

--- apps/teacher/src/app.tsx
+++ apps/teacher/src/app.tsx
@@ -1,6 +1,8 @@
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { indigo, pink } from '@mui/material/colors';
+import { useTitle } from 'ahooks';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Routers from './routers';
@@ -8,6 +10,10 @@ import Routers from './routers';
 const theme = createTheme({ palette: { primary: indigo, secondary: pink } });
 
 const App: React.FC = () => {
+  const { t } = useTranslation();
+
+  useTitle(t('layout.title'));
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
