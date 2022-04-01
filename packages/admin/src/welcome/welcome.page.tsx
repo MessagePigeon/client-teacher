5c7233fe59d1d706883da0dd75564cd9f336a19e
Justin Sun <i@jsun969.cn>
2022-04-01 11:25:48 +0800

    feat(admin): add welcome text for welcome page

--- packages/admin/src/welcome/welcome.page.tsx
+++ packages/admin/src/welcome/welcome.page.tsx
@@ -1,7 +1,12 @@
+import { Typography } from '@mui/material';
 import React from 'react';
 
 const WelcomePage: React.FC = () => {
-  return <div>WelcomePage</div>;
+  return (
+    <Typography variant="h2" align="center">
+      Welcome
+    </Typography>
+  );
 };
 
 export default WelcomePage;
