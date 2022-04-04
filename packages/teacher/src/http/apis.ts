11e5491e14181b52e3ab6784f0ad325b1941c2a9
Justin Sun <i@jsun969.cn>
2022-04-04 14:21:16 +0800

    style: remove extra blank lines in apis

--- packages/teacher/src/http/apis.ts
+++ packages/teacher/src/http/apis.ts
@@ -23,47 +23,36 @@ export class API {
   static async login(body: LoginRequest) {
     return await service.post<LoginResponse>('/login', body);
   }
-
   static async register(body: RegisterRequest) {
     return await service.post('/register', body);
   }
-
   static async init() {
     return await service.get<InitResponse>('/init');
   }
-
   static async getStudents() {
     return await service.get<StudentResponse>('/students');
   }
-
   static async connectStudent(body: ConnectRequest) {
     return await service.post<ConnectRequestResponse>('/connect-request', body);
   }
-
   static async modifyStudentRemark(body: ModifyStudentRemarkRequest) {
     return await service.patch('/student/remark', body);
   }
-
   static async deleteStudent(studentId: string) {
     return await service.delete('/student', { params: { studentId } });
   }
-
   static async modifyName(body: ModifyNameRequest) {
     return await service.patch('/name', body);
   }
-
   static async modifyPassword(body: ModifyPasswordRequest) {
     return await service.patch('/password', body);
   }
-
   static async sendMessage(body: SendMessageRequest) {
     return await service.post<SendMessageResponse>('/message', body);
   }
-
   static async getMessages(params: PaginationRequest) {
     return await service.get<MessagesResponse>('/messages', { params });
   }
-
   static async closeMessage(body: CloseMessageRequest) {
     return await service.post('/message-close', body);
   }
