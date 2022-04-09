615122d47e7882f7c04b7abb34ccd6aea88605d2
Justin Sun <i@jsun969.cn>
2022-04-09 17:40:55 +0800

    refactor(admin): package user card component

---
+++ packages/admin/src/common/components/user-card.component.tsx
@@ -0,0 +1,85 @@
+import {
+  Box,
+  Divider,
+  IconButton,
+  Link,
+  Paper,
+  Stack,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+import { useBoolean } from 'ahooks';
+import React from 'react';
+import CopyableChip from './copyable-chip.component';
+
+type Action = { icon: React.ReactNode; tooltip: string; onClick: () => void };
+
+interface UserCardProps {
+  id: string;
+  online: boolean;
+  connectedUsers: Array<{ name: string; id: string }>;
+  actions: Action[];
+}
+
+const UserCard: React.FC<UserCardProps> = ({
+  id,
+  online,
+  children,
+  connectedUsers,
+  actions,
+}) => {
+  const [isIdCopied, { set: setIsIdCopied }] = useBoolean();
+
+  return (
+    <Paper sx={{ p: 2, display: 'flex' }} variant="outlined">
+      <Box sx={{ flexGrow: 1 }}>
+        <Box>
+          <Tooltip
+            title={isIdCopied ? 'Copied!' : 'Copy'}
+            placement="top"
+            arrow
+          >
+            <Link
+              color="text.secondary"
+              variant="caption"
+              component="button"
+              onClick={async () => {
+                await navigator.clipboard.writeText(id);
+                setIsIdCopied(true);
+                setTimeout(() => setIsIdCopied(false), 1000);
+              }}
+            >
+              {id}
+            </Link>
+          </Tooltip>
+          <Typography
+            color={online ? 'success.main' : 'error.main'}
+            variant="caption"
+            sx={{ ml: 0.5 }}
+          >
+            {online ? 'Online' : 'Offline'}
+          </Typography>
+        </Box>
+        <Box>{children}</Box>
+        {connectedUsers.map((user) => (
+          <CopyableChip key={user.id} copyValue={user.id}>
+            {user.name}
+          </CopyableChip>
+        ))}
+      </Box>
+      <Divider orientation="vertical" flexItem sx={{ mx: 0.5 }} />
+      <Stack
+        justifyContent="center"
+        divider={<Divider flexItem sx={{ my: 0.5 }} />}
+      >
+        {actions.map(({ tooltip, icon, onClick }, index) => (
+          <Tooltip key={index} title={tooltip} placement="right" arrow>
+            <IconButton onClick={onClick}>{icon}</IconButton>
+          </Tooltip>
+        ))}
+      </Stack>
+    </Paper>
+  );
+};
+
+export default UserCard;
--- packages/admin/src/teacher/components/teacher-card.component.tsx
+++ packages/admin/src/teacher/components/teacher-card.component.tsx
@@ -1,18 +1,9 @@
 import { Delete, DriveFileRenameOutline, Password } from '@mui/icons-material';
-import {
-  Box,
-  Divider,
-  IconButton,
-  Link,
-  Paper,
-  Stack,
-  Tooltip,
-  Typography,
-} from '@mui/material';
+import { Typography } from '@mui/material';
 import { useBoolean, useRequest } from 'ahooks';
 import React from 'react';
 import { toast } from 'react-toastify';
-import CopyableChip from '~/common/components/copyable-chip.component';
+import UserCard from '~/common/components/user-card.component';
 import { confirmToast } from '~/common/helpers/confirm-toast.helper';
 import { API } from '~/http/apis';
 import { Teacher } from '~/http/types';
@@ -25,7 +16,6 @@ interface TeacherCardProps {
 }
 
 const TeacherCard: React.FC<TeacherCardProps> = ({ teacher, onRefresh }) => {
-  const [isIdCopied, { set: setIsIdCopied }] = useBoolean();
   const [
     isResetPasswordSuccessDialogOpen,
     {
@@ -56,84 +46,51 @@ const TeacherCard: React.FC<TeacherCardProps> = ({ teacher, onRefresh }) => {
     },
   });
 
+  const actions = [
+    {
+      tooltip: 'Reset Password',
+      icon: <Password />,
+      onClick() {
+        confirmToast(`Are you sure to reset password for ${teacher.name}`, () =>
+          runResetPassword({ id: teacher.id }),
+        );
+      },
+    },
+    {
+      tooltip: 'Modify Name',
+      icon: <DriveFileRenameOutline />,
+      onClick: openRenameDialog,
+    },
+    {
+      tooltip: 'Delete',
+      icon: <Delete />,
+      onClick() {
+        confirmToast(`Are you sure to delete ${teacher.name}`, () =>
+          runDelete({ id: teacher.id }),
+        );
+      },
+    },
+  ];
+
   return (
-    <Paper sx={{ p: 2, display: 'flex' }} variant="outlined">
-      <Box sx={{ flexGrow: 1 }}>
-        <Box>
-          <Tooltip
-            title={isIdCopied ? 'Copied!' : 'Copy'}
-            placement="top"
-            arrow
-          >
-            <Link
-              color="text.secondary"
-              variant="caption"
-              component="button"
-              onClick={async () => {
-                await navigator.clipboard.writeText(teacher.id);
-                setIsIdCopied(true);
-                setTimeout(() => setIsIdCopied(false), 1000);
-              }}
-            >
-              {teacher.id}
-            </Link>
-          </Tooltip>
-          <Typography
-            color={teacher.online ? 'success.main' : 'error.main'}
-            variant="caption"
-            sx={{ ml: 0.5 }}
-          >
-            {teacher.online ? 'Online' : 'Offline'}
-          </Typography>
-        </Box>
-        <Box>
-          <Typography>
-            <strong>Name:</strong> {teacher.name}
-          </Typography>
-          <Typography>
-            <strong>Username:</strong> {teacher.username}
-          </Typography>
-        </Box>
-        {teacher.students.map((student) => (
-          <CopyableChip key={student.id} copyValue={student.id}>
-            {student.defaultRemark}
-          </CopyableChip>
-        ))}
-      </Box>
-      <Divider orientation="vertical" flexItem sx={{ mx: 0.5 }} />
-      <Stack
-        justifyContent="center"
-        divider={<Divider flexItem sx={{ my: 0.5 }} />}
+    <>
+      <UserCard
+        id={teacher.id}
+        online={teacher.online}
+        connectedUsers={teacher.students.map((student) => ({
+          id: student.id,
+          name: student.defaultRemark,
+        }))}
+        actions={actions}
       >
-        <Tooltip title="Reset Password" placement="right" arrow>
-          <IconButton
-            onClick={() => {
-              confirmToast(
-                `Are you sure to reset password for ${teacher.name}`,
-                () => runResetPassword({ id: teacher.id }),
-              );
-            }}
-          >
-            <Password />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Modify name" placement="right" arrow>
-          <IconButton onClick={openRenameDialog}>
-            <DriveFileRenameOutline />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Delete" placement="right" arrow>
-          <IconButton
-            onClick={() => {
-              confirmToast(`Are you sure to delete ${teacher.name}`, () =>
-                runDelete({ id: teacher.id }),
-              );
-            }}
-          >
-            <Delete />
-          </IconButton>
-        </Tooltip>
-      </Stack>
+        <Typography>
+          <strong>Name:</strong> {teacher.name}
+        </Typography>
+        <Typography>
+          <strong>Username:</strong> {teacher.username}
+        </Typography>
+      </UserCard>
+
       <UserNamePasswordDialog
         open={isResetPasswordSuccessDialogOpen}
         title="Reset Password Success"
@@ -141,6 +98,7 @@ const TeacherCard: React.FC<TeacherCardProps> = ({ teacher, onRefresh }) => {
         username={resetPasswordData?.data.username || ''}
         password={resetPasswordData?.data.newPassword || ''}
       />
+
       <RenameDialog
         open={isRenameDialogOpen}
         onClose={closeRenameDialog}
@@ -151,7 +109,7 @@ const TeacherCard: React.FC<TeacherCardProps> = ({ teacher, onRefresh }) => {
           onRefresh();
         }}
       />
-    </Paper>
+    </>
   );
 };
 
