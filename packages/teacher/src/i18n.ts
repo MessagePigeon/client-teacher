553ddd09f0a3dc6935179fe4ce7a4ba3c2ded239
Justin Sun <i@jsun969.cn>
2022-04-11 07:54:03 +0800

    style: sort i18n imports

--- packages/teacher/src/i18n.ts
+++ packages/teacher/src/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import en from './locales/en.json';
 import zh from './locales/zh.json';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
   .use(initReactI18next)
