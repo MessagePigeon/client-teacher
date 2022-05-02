b66edabf4d02af2371bd1f8684bd21c47385f45b
Justin Sun <i@jsun969.cn>
2022-05-02 23:01:27 +0800

    feat: add pwa

--- packages/teacher/src/main.tsx
+++ packages/teacher/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { registerSW } from 'virtual:pwa-register';
 import App from './app';
 import './i18n';
 import { store } from './state/store';
+
+registerSW({ immediate: true });
 
 ReactDOM.render(
   <React.StrictMode>
